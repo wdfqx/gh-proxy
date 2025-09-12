a981bbe904b8ba236c41091d6bb1927ef4fb3145
wdfqx <wdfqx@outlook.com>
2025-09-13 00:34:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 /**
  * static files (404.html, sw.js, conf.js)
  */
-const ASSET_URL = 'https://hunshcn.github.io/gh-proxy/'
+const ASSET_URL = 'https://wdfqx.github.io/gh-proxy/'
 // 前缀，如果自定义路由为example.com/gh/*，将PREFIX改为 '/gh/'，注意，少一个杠都会错！
 const PREFIX = '/'
 
